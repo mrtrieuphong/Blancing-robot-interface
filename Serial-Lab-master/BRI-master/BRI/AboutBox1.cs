50ab0ff76b163a8adf07078ed2c3a9d158ee5822
ICNIVAD <39859009+PhongCode@users.noreply.github.com>
2020-05-11 20:05:38 +0700

    vesion 01
    nguyen trieu phong

---
+++ Serial-Lab-master/BRI-master/BRI/AboutBox1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BRI
+{
+    partial class AboutBox1 : Form
+    {
+        public AboutBox1()
+        {
+
+            InitializeComponent();
+            
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://nguyentrieuphong.com");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AboutBox1_Load(object sender, EventArgs e)
+        {}
+    }
+}
---
+++ Serial-Lab-master/BRI-master/BRI/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BRI")]
+[assembly: AssemblyDescription("Phần mềm phục vụ môn học Đo Lường và Điều Khiển trên máy tính")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nguyen Trieu Phong")]
+[assembly: AssemblyProduct("Blancing Robot Interface")]
+[assembly: AssemblyCopyright("Copyright © 2020 - Nguyen Trieu Phong")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("28916f23-4251-4aa5-8e03-d21d33510fb6")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
